609f70351a0524ba2a884a14d81f8bca95135a24
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-07-18 22:10:45 +0300

    created app with marko.js, lasso

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import dataSet from "../../dataset.json"
+import template from "./template.marko";
+
+
+export default (req, res) => {
+  res.marko(template, {
+    pageMeta: dataSet.page_meta,
+    navs: dataSet.nav,
+    breadcrumbs: dataSet.breadcrumbs,
+    stocks: dataSet.stock,
+    pageTexts: dataSet.page_text,
+  });
+};
