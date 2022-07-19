d92d5ae659c6a0dae9dfa7199e8e0f8ee4cc061f
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-07-19 22:23:24 +0300

    changed the package manager to webpack

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,5 @@
-import dataSet from "../../dataset.json"
 import template from "./template.marko";
+import dataSet from "../../dataset.json"
 
 export default (req, res) => {
   res.marko(template, {
